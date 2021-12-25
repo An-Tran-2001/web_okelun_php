70a238d2a5d8b0a31ee24d69175c41b29d1f9817
An-Tran-2001 <93371117+An-Tran-2001@users.noreply.github.com>
2021-12-26 03:23:19 +0700

    dowlaod chưa có reset lười viết lại

---
+++ pages/dowload/dowload_lol.php
@@ -0,0 +1,57 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dowload.css">
+    <title>DOWLOAD</title>
+</head>
+
+<body>
+    <div class="main">
+        <div class="logo_lol">
+            <div class="img_logo"><img src="img/lol-logo (1).png" alt=""></div>
+            <div class="language">
+                <div class="languegua_str">
+                    <p>言語</p>
+                </div>
+                <select name="" id="select_languague">
+                    <option value="">한국어</option>
+                    <option value="">English</option>
+                    <option value="">日本語</option>
+                </select>
+            </div>
+        </div>
+        <div class="title_lol_dowload">
+            <div class="header_title">
+                <h1>今すぐリーグ・オブ・レジェンドをプレイ</h1>
+            </div>
+            <div class="footer_title">
+                <img src="img/divider-download.png" alt="">
+            </div>
+        </div>
+        <div class="button_dowload">
+            <div class="border_button_lol">
+                <div class="button_lol"
+                    onclick="window.location.href='https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.jp.exe'">
+                    <button class="dowload_lol">Windows版をダウンロード</button>
+                </div>
+            </div>
+            <div class="button_dowload_ios">
+                <a class="link"
+                    href="https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.jp.zip">Mac版インストーラー</a>
+                <a class="link"
+                    href="https://signup.jp.leagueoflegends.com/ja/signup/index?page_referrer=redownload">アカウントを作成</a>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="img_footer">
+                <img src="img/riotgames-logo-white-s.png">
+            </div>
+            <p>このサイトはhCaptchaで保護されており、hCaptchaの<a href="https://hcaptcha.com/privacy" target="_blank">プライバシーポリシー</a>と<a
+                    href="https://hcaptcha.com/terms" target="_blank">サービス規約</a>が適用されます。</p>
+            <p>© 2021 Riot Games, inc. All rights reserved.</p>
+        </div>
+    </div>
+</body>
+
+</html>
